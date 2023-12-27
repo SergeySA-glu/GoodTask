e81da61d6df780188d618cd831aa3786566ec4c5
Сергей Шихалев <glukchek@gmail.com>
2023-12-28 00:07:28 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GoodTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vacationDictionary = new Dictionary<string, List<DateTime>>();
+            var workers = new List<string>()
+            {
+                "Иванов Иван Иванович",
+                "Петров Петр Петрович",
+                "Юлина Юлия Юлиановна",
+                "Сидоров Сидор Сидорович",
+                "Павлов Павел Павлович",
+                "Георгиев Георг Георгиевич"
+            };
+
+            foreach (var worker in workers)
+                vacationDictionary[worker] = VacationGenerator.GetVacationList();
+
+            foreach (var vacationList in vacationDictionary)
+                Console.WriteLine($"Дни отпуска {vacationList.Key} :\n{string.Join("\n", vacationList.Value.Select(date => date.ToShortDateString()))}\n");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ VacationGenerator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GoodTask
+{
+    public static class VacationGenerator
+    {
+        const int AllVacationsPause = 3;
+        const int WorkerVacationsPause = 1;
+
+        static readonly List<DateTime> vacations;
+        static readonly Random gen;
+        static readonly List<int> vacationSteps;
+
+        static VacationGenerator()
+        {
+            vacations = new List<DateTime>();
+            gen = new Random();
+            vacationSteps = new List<int> { 7, 14 };
+        }
+
+        public static List<DateTime> GetVacationList()
+        {
+            var dateList = new List<DateTime>();
+
+            var start = new DateTime(DateTime.Now.Year, 1, 1);
+            var end = new DateTime(DateTime.Now.Year, 12, 31);
+            var range = (end - start).Days;
+            var vacationCount = 28;
+
+            while (vacationCount > 0)
+            {
+                var startDate = start.AddDays(gen.Next(range));
+
+                if (startDate.DayOfWeek == DayOfWeek.Sunday || startDate.DayOfWeek == DayOfWeek.Saturday)
+                    continue;
+
+                var endDate = GetEndDate(startDate, vacationCount == 7, out var difference);
+
+                if (CanCreateVacation(dateList, startDate, endDate))
+                {
+                    for (var dt = startDate; dt < endDate; dt = dt.AddDays(1))
+                    {
+                        vacations.Add(dt);
+                        dateList.Add(dt);
+                    }
+                    vacationCount -= difference;
+                }
+            }
+            return dateList;
+        }
+
+        private static DateTime GetEndDate(DateTime startDate, bool oneWeek, out int difference)
+        {
+            var vacIndex = oneWeek ? 0 : gen.Next(vacationSteps.Count);
+            difference = vacationSteps[vacIndex];
+            return startDate.AddDays(difference);
+        }
+
+        private static bool CanCreateVacation(List<DateTime> dateList, DateTime startDate, DateTime endDate)
+        {
+            if (!vacations.Any(element => element.AddDays(AllVacationsPause) >= startDate && element.AddDays(-AllVacationsPause) <= endDate))
+            {
+                return !dateList.Any(element => element.AddMonths(WorkerVacationsPause) >= startDate
+                    && element.AddMonths(-WorkerVacationsPause) <= endDate);
+            }
+            return false;
+        }
+    }
+}
